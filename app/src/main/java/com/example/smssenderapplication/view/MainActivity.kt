50c84744ed0fe2f3e9a0c3106eae05cfbbec5a29
ImMohMol <mhoseinmola1380@gmail.com>
2022-11-28 21:06:28 +0330

    Improvment: 1. getting contacts from storage completed 2. getting permissions completed

--- app/src/main/java/com/example/smssenderapplication/view/MainActivity.kt
+++ app/src/main/java/com/example/smssenderapplication/view/MainActivity.kt
@@ -26,12 +26,20 @@ class MainActivity : SmsSenderBaseActivity() {
 
     private fun getPermissions() {
         Dexter.withContext(this)
-            .withPermissions(Manifest.permission.READ_CONTACTS)
+            .withPermissions(
+                Manifest.permission.READ_CONTACTS,
+                Manifest.permission.SEND_SMS
+            )
             .withListener(object : MultiplePermissionsListener {
                 override fun onPermissionsChecked(permissonsReport: MultiplePermissionsReport?) {
                     permissonsReport?.let {
                         if (it.areAllPermissionsGranted()) {
                             ContactsExtractor.extractContacts("والدین کانون", this@MainActivity)
+                            val phoneNumbers = StringBuilder()
+                            ContactsExtractor.contactPhoneNumbers.forEach { (key, value) ->
+                                phoneNumbers.append(String.format("%s -> %s\n", key, value))
+                            }
+                            this@MainActivity.binding.tvContactName.text = phoneNumbers.toString()
                         } else {
                             this@MainActivity.createSnackBar(
                                 getString(R.string.permissions_needed),
